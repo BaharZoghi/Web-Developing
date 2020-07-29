c8b0d52f80b55c855896bd9d20d4b058e18307d3
ZeinabZoghi <bh.zoghi@hotmail.com>
2020-07-29 11:01:30 -0400

    Nostalgia

--- Nostalgia/js/gallery.js
+++ Nostalgia/js/gallery.js
@@ -9,13 +9,13 @@ function tplawesome(e,t){
 	}
 
 
-function authenticate() {
-    return gapi.auth2.getAuthInstance()
+// function authenticate() {
+    // return 
+    gapi.auth2.getAuthInstance()
         .signIn({scope: "https://www.googleapis.com/auth/youtube.force-ssl"})
         .then(function() { console.log("Sign-in successful"); },
               function(err) { console.error("Error signing in", err); });
-}
-window.onload= authenticate;
+// }
 
 
 $(function(){
